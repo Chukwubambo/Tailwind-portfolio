f9495aa689b6a24191d757d8c50d7fa22e3725cd
Adebambo Omolola <adebamboomolola31@gmail.com>
2024-06-07 08:18:54 +0100

    Tailwind Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"class",
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+    
+      spacing:{
+        "big":"48rem"
+      }
+    },
+
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+
+    },
+
+    fontFamily:{
+      Nunito:["Nunito", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
+
